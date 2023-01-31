09586937088c6b49dcbe3a57b6293e886cd1cb29
dev <tipusaeed501@gmail.com>
2023-01-31 07:39:53 -0600

    Added navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
+"use client";
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import Navbar from '@/Components/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      Panaverse Syllabus
+      <Navbar />
     </main>
   )
 }
