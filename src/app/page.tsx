37722384a971d6b37a78c15153fe97e6ae0f0ab9
dev <tipusaeed501@gmail.com>
2023-01-31 16:13:05 -0600

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 "use client";
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from './page.module.css'
 import Navbar from '@/Components/navbar'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
